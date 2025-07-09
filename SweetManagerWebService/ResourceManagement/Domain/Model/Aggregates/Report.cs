dc7af5c30c99d90c2551f3ddcc59c2c9d5806a2f
Sebastian EP <149455319+sebasepe@users.noreply.github.com>
2025-07-09 07:56:58 -0500

    feat : add comments report

--- SweetManagerWebService/ResourceManagement/Domain/Model/Aggregates/Report.cs
+++ SweetManagerWebService/ResourceManagement/Domain/Model/Aggregates/Report.cs
@@ -1,23 +1,43 @@
-﻿using SweetManagerWebService.IAM.Domain.Model.Aggregates;
+using SweetManagerWebService.IAM.Domain.Model.Aggregates;
 using SweetManagerWebService.ResourceManagement.Domain.Model.Commands;
 using SweetManagerWebService.ResourceManagement.Domain.Model.Entities;
 
 namespace SweetManagerWebService.ResourceManagement.Domain.Model.Aggregates
 {
+    // Represents a report entity with associated properties and constructors
     public partial class Report
     {
+        // Unique identifier for the report
         public int Id { get; }
+        
+        // Foreign key to the type of report
         public int TypesReportsId { get; set; }
+        
+        // Foreign key to the admin who created the report
         public int AdminsId { get; set; }
+        
+        // Foreign key to the worker associated with the report
         public int WorkersId { get; set; }
+        
+        // URL of the file associated with the report
         public string FileUrl { get; set; } = null!;
+        
+        // Title of the report
         public string Title { get; set; } = null!;
+        
+        // Description of the report
         public string Description { get; set; } = null!;
 
+        // Navigation property to the associated admin
         public virtual Admin Admin { get; } = null!;
+        
+        // Navigation property to the associated type report
         public virtual TypeReport TypeReport { get; } = null!;
+        
+        // Navigation property to the associated worker
         public virtual Worker Worker { get; } = null!;
 
+        // Default constructor initializing properties to default values
         public Report()
         {
             this.TypesReportsId = 0;
@@ -28,6 +48,7 @@ namespace SweetManagerWebService.ResourceManagement.Domain.Model.Aggregates
             this.Description = string.Empty;
         }
 
+        // Constructor to initialize report with specific values
         public Report(int typesReportsId, int adminsId, int workersId, string fileUrl, string title, string description)
         {
             this.TypesReportsId = typesReportsId;
@@ -38,6 +59,7 @@ namespace SweetManagerWebService.ResourceManagement.Domain.Model.Aggregates
             this.Description = description;
         }
 
+        // Constructor to create a report from a command object (CreateReportCommand)
         public Report(CreateReportCommand command)
         {
             this.TypesReportsId = command.TypesReportsId;
