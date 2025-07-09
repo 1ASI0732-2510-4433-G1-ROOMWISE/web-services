1c8305872ac199dfc488777f14280b6b82bdc3d2
Sebastian EP <149455319+sebasepe@users.noreply.github.com>
2025-07-09 07:58:30 -0500

    feat : add comments User.cs

--- SweetManagerWebService/Shared/Domain/Model/Entities/User.cs
+++ SweetManagerWebService/Shared/Domain/Model/Entities/User.cs
@@ -1,17 +1,21 @@
-﻿using SweetManagerWebService.Shared.Domain.Model.ValueObjects;
+using SweetManagerWebService.Shared.Domain.Model.ValueObjects;
 
 namespace SweetManagerWebService.Shared.Domain.Model.Entities;
 
 public class User(string email, string name, string surname, string phoneNumber)
 {
+    // Unique identifier for the user
     public int Id { get; set; }
     
+    // Email address of the user, set during initialization
     public string Email { get; private set; } = email;
 
+    // Full name of the user, encapsulated in the CompleteName value object
     public CompleteName Name { get; private set; } = new CompleteName(name, surname);
 
+    // Phone number of the user
     public string PhoneNumber { get; private set; } = phoneNumber;
 
-    public User() : this(string.Empty, string.Empty,  string.Empty, string.Empty){ }
-    
+    // Default constructor initializing the user with empty values
+    public User() : this(string.Empty, string.Empty, string.Empty, string.Empty) { }
 }
