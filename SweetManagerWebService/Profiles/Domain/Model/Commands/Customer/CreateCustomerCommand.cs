f0e84a96f67a4418ea007680e61ef8fc22aabaf2
Sebastian EP <149455319+sebasepe@users.noreply.github.com>
2025-07-09 02:39:19 -0500

    feat : add coments

--- SweetManagerWebService/Profiles/Domain/Model/Commands/Customer/CreateCustomerCommand.cs
+++ SweetManagerWebService/Profiles/Domain/Model/Commands/Customer/CreateCustomerCommand.cs
@@ -1,11 +1,25 @@
-﻿namespace SweetManagerWebService.Profiles.Domain.Model.Commands.Customer;
+namespace SweetManagerWebService.Profiles.Domain.Model.Commands.Customer;
 
+// Class that defines the command to create a new customer in the system
 public record CreateCustomerCommand(
+    // Unique ID of the customer
     int Id, 
+    
+    // Username of the customer
     string Username,
+    
+    // First name of the customer
     string Name, 
+    
+    // Surname of the customer
     string Surname, 
+    
+    // Email address of the customer
     string Email, 
-    int  Phone, 
+    
+    // Phone number of the customer
+    int Phone, 
+    
+    // Status indicating whether the customer is active or inactive
     string State
-    );
+);
