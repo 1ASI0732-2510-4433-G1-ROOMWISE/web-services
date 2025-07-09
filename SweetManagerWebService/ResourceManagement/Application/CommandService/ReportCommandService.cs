42b98c0bd943ddd22768679b3c70fe2027908617
Sebastian EP <149455319+sebasepe@users.noreply.github.com>
2025-07-09 03:18:58 -0500

    feat : add coments

--- SweetManagerWebService/ResourceManagement/Application/CommandService/ReportCommandService.cs
+++ SweetManagerWebService/ResourceManagement/Application/CommandService/ReportCommandService.cs
@@ -5,18 +5,26 @@ using SweetManagerWebService.ResourceManagement.Domain.Services.Report;
 
 namespace SweetManagerWebService.ResourceManagement.Application.CommandService;
 
+// Service class to handle report creation commands
 public class ReportCommandService (IReportRepository reportRepository, IUnitOfWork unitOfWork) : IReportCommandService
 {
+    // Method to handle the creation of a new report
     public async Task<bool> Handle(CreateReportCommand command)
     {
         try
         {
+            // Adds the new report to the repository asynchronously
             await reportRepository.AddAsync(new (command));
+            
+            // Completes the unit of work to persist changes to the database
             await unitOfWork.CompleteAsync();
+            
+            // Returns true if the report was successfully created
             return true;
         }
         catch (Exception)
         {
+            // Returns false in case of an error
             return false;
         }
     }
