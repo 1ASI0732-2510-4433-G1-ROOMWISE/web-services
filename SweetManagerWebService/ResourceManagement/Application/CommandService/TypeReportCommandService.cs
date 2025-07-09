9f81967a9575c06de1660bc1e818e5af61d52e69
Sebastian EP <149455319+sebasepe@users.noreply.github.com>
2025-07-09 07:55:54 -0500

    feat : add comments typereport

--- SweetManagerWebService/ResourceManagement/Application/CommandService/TypeReportCommandService.cs
+++ SweetManagerWebService/ResourceManagement/Application/CommandService/TypeReportCommandService.cs
@@ -1,4 +1,4 @@
-﻿using sweetmanager.API.Shared.Domain.Repositories;
+using sweetmanager.API.Shared.Domain.Repositories;
 using SweetManagerWebService.ResourceManagement.Domain.Model.Commands;
 using SweetManagerWebService.ResourceManagement.Domain.Model.Entities;
 using SweetManagerWebService.ResourceManagement.Domain.Model.ValueObjects;
@@ -7,21 +7,28 @@ using SweetManagerWebService.ResourceManagement.Domain.Services.TypeReport;
 
 namespace SweetManagerWebService.ResourceManagement.Application.CommandService;
 
+// Service class to handle type report-related commands
 public class TypeReportCommandService(ITypeReportRepository typeReportRepository,
     IUnitOfWork unitOfWork) : ITypeReportCommandService
 {
+    // Method to handle seeding of type reports into the repository
     public async Task<bool> Handle(SeedTypeReportsCommand command)
     {
+        // Loop through all values in the ETypeReports enum
         foreach (var typeReport in Enum.GetValues(typeof(ETypeReports)))
         {
+            // Check if the type report already exists in the repository
             if (await typeReportRepository.FindByNameAsync(typeReport.ToString()!) is false)
             {
+                // Add the type report to the repository if it doesn't exist
                 await typeReportRepository.AddAsync(new TypeReport(typeReport.ToString()!));
             }
         }
 
+        // Completes the unit of work to persist changes to the database
         await unitOfWork.CompleteAsync();
         
+        // Returns true if the seed operation was successful
         return true;
     }
 }
